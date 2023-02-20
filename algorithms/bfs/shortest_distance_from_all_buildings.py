72497893d899ddf790501233db23b50f2445b8ec
MrZoiya <savioz@kth.se>
2023-02-20 16:44:22 +0100

    Manual instrumentation of the shortest distance function. Closes #14

--- algorithms/bfs/shortest_distance_from_all_buildings.py
+++ algorithms/bfs/shortest_distance_from_all_buildings.py
@@ -6,25 +6,42 @@ when grid[i][j] == -b_nums, it means that grid[i][j] are already visited from al
 and use dist to record distances from b_nums
 """
 
+instrument = [0] * 12
+
 def shortest_distance(grid):
+    global instrument
     if not grid or not grid[0]:
+        instrument[0] = 1
         return -1
+    else:
+        instrument[1] = 1
 
     matrix = [[[0,0] for i in range(len(grid[0]))] for j in range(len(grid))]
 
     count = 0    # count how many building we have visited
     for i in range(len(grid)):
+        instrument[2] = 1
         for j in range(len(grid[0])):
+            instrument[3] = 1
             if grid[i][j] == 1:
+                instrument[4] = 1
                 bfs(grid, matrix, i, j, count)
                 count += 1
+            else:
+                instrument[5] = 1
 
     res = float('inf')
     for i in range(len(matrix)):
+        instrument[6] = 1
         for j in range(len(matrix[0])):
+            instrument[7] = 1
             if matrix[i][j][1]==count:
+                instrument[8] = 1
                 res = min(res, matrix[i][j][0])
+            else:
+                instrument[9] = 1
 
+    instrument[10] = 1 if res!=float('inf') else instrument[11] = 1
     return res if res!=float('inf') else -1
 
 def bfs(grid, matrix, i, j, count):
@@ -38,3 +55,15 @@ def bfs(grid, matrix, i, j, count):
                 matrix[k][l][0] += step+1
                 matrix[k][l][1] = count+1
                 q.append((k, l, step+1))
+
+def checkcoverage():
+    global instrument
+    coverage = sum(instrument) / len(instrument) * 100
+
+    print("----------Current Branch Coverage----------")
+    print("Unreachable: 0")
+    print("Branch coverage:", coverage, "%")
+    print("----------Current Branch Coverage----------")
+
+if __name__ == '__main__':
+    checkcoverage()
